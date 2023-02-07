2fec1969a7e8846bd4082b72fd42168d701bc7b2
AbdulhafizovPulat <pulatabdulhafizov2003@gmail.com>
2023-02-07 14:59:50 +0300

    render

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
 const elList = document.querySelector("#content");
 const elSearch = document.querySelector("#search");
 
-films.forEach((film) => {
-  const otherDate = new Date(film.release_date);
-  const li = document.createElement("li");
-  // create li
-  li.innerHTML = `
+function renderFilm(array) {
+  for (let i = 0; i < array.length; i++) {
+    const otherDate = new Date(array[i].release_date);
+    const li = document.createElement("li");
+    // create li
+    li.innerHTML = `
         <li class="list__item">
-            <h5>${film.title}</h5>
-            <img class="list__img" src=${film.poster} alt="">
-            <p class="info">${film.overview}</p>
+            <h5>${array[i].title}</h5>
+            <img class="list__img" src=${array[i].poster} alt="">
+            <p class="info">${array[i].overview}</p>
             <span>Release: ${otherDate}</span>
-            <button data-id="${film.id}" class="btn">Delete</button>
+            <button data-id="${array[i].id}" class="btn">Delete</button>
         </li>
     `;
-  const ul = document.createElement("ul");
+    const ul = document.createElement("ul");
 
-  // genres section
-  film.genres.forEach((genre) => {
-    const p = document.createElement("p");
-    p.textContent = genre;
-    p.className = "genries";
+    // genres section
+    array[i].genres.forEach((genre) => {
+      const p = document.createElement("p");
+      p.textContent = genre;
+      p.className = "genries";
 
-    ul.appendChild(p);
-  });
-  li.appendChild(ul);
+      ul.appendChild(p);
+    });
+    li.appendChild(ul);
 
-  elList.appendChild(li);
+    elList.appendChild(li);
+  }
+}
 
+renderFilm(films);
+films.forEach((film) => {
   // delete
 
   li.addEventListener("click", function (evt) {
